a98a76eb577bbf9588778101ab99bbc7fd120abd
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-06-13 15:42:38 +0100

    4./5. Power Saving Mode created

--- jasmine-standalone-3.5.0_new/spec/FeatureSpec.js
+++ jasmine-standalone-3.5.0_new/spec/FeatureSpec.js
@@ -16,10 +16,25 @@ describe("Feature test:" , function(){
   });
   
   it("3. You can decrease the temperature with a down function", function(){
-    expect(thermostat.down()).toBeLessThan(20);
+    thermostat.down()
+    expect(thermostat.getCurrentTemperature()).toBeLessThan(20);
   });
 
+  it("4. The minimum temperature is 10 degrees", function(){
+    for (var i = 0; i<11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
+  it("5. Has power saving mode (PSM) on by default", function(){
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  })
   
+  it("5.1 PSM can be turned off", function(){
+    thermostat.switchPowerSavingModeOff()
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
 });
 
 
--- jasmine-standalone-3.5.0_new/src/Thermostat.js
+++ jasmine-standalone-3.5.0_new/src/Thermostat.js
@@ -2,18 +2,39 @@
 
 class Thermostat { 
   constructor(){
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.psm = true
   }
 
   getCurrentTemperature(){
     return this.temperature;
   };  
 
+  isPowerSavingModeOn(){
+    return this.psm === true;
+  }
+
   up(){
     return this.temperature += 1;  
   };  
 
   down(){
-    return this.temperature -= 1;
+    if( this.isMinimumTemp()) {
+      return;
+    }
+   return  this.temperature -= 1
   };
+
+  isMinimumTemp(){
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isPowerSavingModeOn() {
+    return this.psm === true;
+  }
+
+  switchPowerSavingModeOff(){
+    this.psm = false
+  }
 };
